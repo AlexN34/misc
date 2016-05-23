59f4d6826e6d2a2a33262e77ba58d4a3fe983a67
AlexN34 <alex-nguyen@live.com.au>
2016-05-23 10:35:47 +1000

    started assignment4

---
+++ 4ass/auto/4ass.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "4ass"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt" "a4paper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("geometry" "margin=0.8in")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "listings"
+    "geometry"
+    "amsmath"
+    "fancyhdr"
+    "graphicx")
+   (LaTeX-add-labels
+    "overflow"))
+ :latex)
+
