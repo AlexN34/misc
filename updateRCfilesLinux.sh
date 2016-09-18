ff957ff0bd635a054686c91b0693a6ab9b3b8150
AlexN34 <alex-nguyen@live.com.au>
2016-09-18 21:24:13 +1000

    Added settings update script

---
+++ updateRCfilesLinux.sh
@@ -0,0 +1,6 @@
+cp $HOME/.vimrc settings/vim/.vimrc
+cp -r $HOME/.vim settings/vim/.vim
+cp $HOME/.bashrc settings/.bashrc
+cp $HOME/.zshrc settings/.zshrc
+cp $HOME/.zshenv settings/.zshenv
+cp $HOME/.tmux.conf settings/.tmux.conf
